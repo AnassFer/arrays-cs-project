11060fbec0264eed82ce349ac845e809016e69d7
AnassFer <fertatanas@gmail.com>
2026-09-23 19:37:39 +0000

    Student made public

--- src/main/java/samplearrays/ManageStudent.java
+++ src/main/java/samplearrays/ManageStudent.java
@@ -5,50 +5,90 @@ import java.util.Comparator;
 
 public class ManageStudent {
 
+    Student[] students = {new Student(1,"Anass"), new Student(2, "Douae", 20), new Student(3, "Ilyass",25, 17), new Student(4, "Amine", 31, 19), new Student(5, "Imane")};
+
     // 2) Find the Oldest Student
     public static Student findOldest(Student[] students) {
-
+        Student oldest = students[0];
+        for(Student student: students){
+            if (student.getAge() > oldest.getAge()){
+                oldest = student;
+            }
+        }
         return oldest;
     }
 
     // 3) Count Adult Students (age >= 18)
     public static int countAdults(Student[] students) {
-
+        int count = 0;
+        for(Student student: students){
+            if (student.getAge() >= 18){
+                count++;
+            }
+        }
+        return count;
     }
 
     // 4) Average Grade (returns NaN if no students or grades)
     public static double averageGrade(Student[] students) {
+        int count = 0;
+        for(Student student: students){
 
+                count+= student.getGrade();
+
+        }
+        return (double) count /students.length;
     }
 
     // 5) Search by Name (case-sensitive; change to equalsIgnoreCase if desired)
     public static Student findStudentByName(Student[] students, String name) {
-
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getName().equalsIgnoreCase(name)) return students[i];
+        }
+        return null;
     }
 
     // 6) Sort Students by Grade (descending)
     public static void sortByGradeDesc(Student[] students) {
-
+        Arrays.sort(students, (s1, s2) -> Integer.compare(s1.getGrade(), s2.getGrade()) );
     }
 
     // 7) Print High Achievers (grade >= 15)
     public static void printHighAchievers(Student[] students) {
-
+        System.out.println("High achievers:");
+        for (Student student : students){
+            if(student.getGrade()>=15) System.out.println(student.getName()+" ");
+        }
     }
 
     // 8) Update Student Grade by id
     public static boolean updateGrade(Student[] students, int id, int newGrade) {
+        for (int i = 0; i < students.length; i++) {
+            if(students[i].getId() == id) {
+                students[i].setGrade(newGrade);
+                return true;}
 
+        }
+        return false;
     }
 
     // 9) Find Duplicate Names
     public static boolean hasDuplicateNames(Student[] students) {
 
+        for(Student student1 : students){
+            for (Student student2 : students){
+                if(student1.getName().equalsIgnoreCase(student2.getName())) return true
+            }
+        }
+        return false;
     }
 
     // 10) Expandable Array: return a new array with one more slot and append student
     public static Student[] appendStudent(Student[] students, Student newStudent) {
-
+        Student[] newStudents ;
+        newStudents = Arrays.copyOf(students, students.length+1);
+        newStudents[newStudents.length-1] = newStudent;
+        return newStudents;
     }
 
     // 1) Create an Array of Students + demos for all tasks
